166a0eb01c92d1b05d66a587b91635f1435dc2ca
Gabriele Greco <gabriele.greco@darts.it>
2012-09-19 12:03:11 +0200

    Initial Commit

---
+++ ed2k-Queue/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ed2k-Queue
+//
+//  Created by gabry on 9/19/12.
+//  Copyright (c) 2012 Gabriele Greco. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ ed2k-Queue/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  ed2k-Queue
+//
+//  Created by gabry on 9/19/12.
+//  Copyright (c) 2012 Gabriele Greco. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
